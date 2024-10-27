7a43ec140e35d3013087fb58429c78267f575ae2
Geoff Apps <825340+gapps@users.noreply.github.com>
2024-10-26 17:11:45 -0700

    Turned off Ruby and added special root prompt

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -1,8 +1,13 @@
-# Display time
-# SPACESHIP_TIME_SHOW=true
+# Spaceship prompt configuration
+# https://spaceship-prompt.sh/config/intro/
 
-# Display username always
+# Core prompt config
+# SPACESHIP_TIME_SHOW=true
 # SPACESHIP_USER_SHOW=always
+# SPACESHIP_CHAR_SYMBOL="$"
+SPACESHIP_CHAR_SYMBOL_ROOT="#"
+# SPACESHIP_CHAR_SUFFIX=" "
+
 
 # Do not truncate path in repos
 SPACESHIP_DIR_TRUNC_REPO=true
@@ -11,3 +16,6 @@ SPACESHIP_PROMPT_ADD_NEWLINE=false
 # Add a custom vi-mode section to the prompt
 # See: https://github.com/spaceship-prompt/spaceship-vi-mode
 # spaceship add --before char vi_mode
+
+# Turn off the languge stuff
+SPACESHIP_RUBY_SHOW=false
